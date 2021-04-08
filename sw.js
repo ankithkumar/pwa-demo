e53cb25fb02bdc19e73cafc10ccf80cbddd25996
ankithkumar <ankith.b01@gmail.com>
2021-04-08 19:17:42 +0530

    modification

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCache = "demo-app-static-v3";
-const dynamicCache = "demo-app-dynamic-v3";
+const staticCache = "demo-app-static-v4";
+const dynamicCache = "demo-app-dynamic-v4";
 
 const OFFLINE_URL = 'offline.html';
 const INIT_PAGE = 'index.html';
